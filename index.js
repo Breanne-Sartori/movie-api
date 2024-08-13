01afb5adb833db2a0b6c0109c093ec07eeff1109
Breanne-Sartori <breanne.sartori@gmail.com>
2024-08-13 21:34:25 +1000

    Movie list & morgan added

--- index.js
+++ index.js
@@ -1,15 +1,50 @@
-const express = require('express');
+const express = require('express'),
+    morgan = require('morgan');
+
 const app = express();
+
+app.use(morgan('common'));
 
 let topMovies = [ 
     {
-    title: ''
+        title: 'L.A Confidential',
+        year: '1997'
     },
     {
-        title: ''
+        title: 'The Godfather',
+        year: '1972'
     },
     {
-        title: ''
+        title: 'Casablanca',
+        year: '1942'
+    },
+    {
+        title: 'Seven Samurai',
+        year: '1954'
+    },
+    {
+        title: 'Parasite',
+        year: '2019'
+    },
+    {
+        title: 'Schindler\'s List',
+        year: '1993'
+    },
+    {
+        title: 'Toy Story',
+        year: '1995'
+    },
+    {
+        title: 'Chinatown',
+        year: '1974'
+    },
+    {
+        title: 'All About Eve',
+        year: '1950'
+    },
+    {
+        title: 'How to Train Your Dragon',
+        year: '2010'
     }
 ];
 
@@ -22,6 +57,11 @@ app.get('/', (req, res) => {
 });
 
 app.use(express.static('public'));
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
+});
 
 app.listen(8080, () => {
     console.log('Your app is listening on port 8080.');
